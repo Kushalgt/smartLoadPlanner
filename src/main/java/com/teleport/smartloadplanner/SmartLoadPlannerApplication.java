320439f90bb199583ca80ab6db3095d9117e0d65
Kushalgt <110596956+Kushalgt@users.noreply.github.com>
2026-04-21 07:58:05 +0530

    Initial Setup

---
+++ src/main/java/com/teleport/smartloadplanner/SmartLoadPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.teleport.smartloadplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLoadPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartLoadPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teleport/smartloadplanner/SmartLoadPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teleport.smartloadplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLoadPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
